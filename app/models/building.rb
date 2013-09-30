f5030f4d5c5b47df45628a36bdaf677cdf9b4d39
libranner <libranner@gmail.com>
2013-09-29 21:34:41 -0400

    Model para classrooms referenciando Building

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,6 +3,7 @@ class Building < ActiveRecord::Base
   validates :abbreviation, length: {is: 2}
   validates :rooms, numericality: {greater_than: 0}
   validates :floors, numericality: {greater_than: 0}
-  #a shot at the night
+  has_many :classrooms
+
 
 end
