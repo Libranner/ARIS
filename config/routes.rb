0524889c0758f97f67b6e8aac54c3c4cf7d2d53f
libranner <libranner@gmail.com>
2013-09-30 23:36:18 -0400

    Add routes for office resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 ARIS::Application.routes.draw do
+  resources :offices
+
+
+  resources :classrooms
+
+
+  resources :buildings
+
+
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => 'home#index'
   devise_for :users
   resources :users
+
 end
