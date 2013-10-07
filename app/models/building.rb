2d3692ea92f0084577cb4aece013701d83a71aab
libranner <libranner@gmail.com>
2013-10-07 01:29:00 -0400

    Modification to accept nested attributes for models office and classroom.

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,8 +1,11 @@
 class Building < ActiveRecord::Base
-  attr_accessible :abbreviation, :floors, :name, :rooms
+  attr_accessible :abbreviation, :floors, :name, :rooms, :classrooms_attributes, :offices_attributes
   validates :abbreviation, length: {is: 2}
   validates :rooms, numericality: {greater_than: 0}
   validates :floors, numericality: {greater_than: 0}
   validates :name, uniqueness: true
   has_many :classrooms
+  has_many :offices
+  accepts_nested_attributes_for :classrooms, allow_destroy: true
+  accepts_nested_attributes_for :offices, allow_destroy: true
 end
